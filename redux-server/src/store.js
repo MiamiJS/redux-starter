e3414675d1aaed07b651a333520e78cd3007e290
wordyallen <joe.narvaez@gmil.com>
2016-05-08 23:47:51 -0400

    added logger

--- redux-server/src/store.js
+++ redux-server/src/store.js
@@ -1,3 +1,16 @@
 // import {createStore,applyMiddleware} from 'redux'
 // import reducer from './reducer'
 // import {firebaseRef} from '../index'
+
+
+
+
+
+
+// export default function(store) {
+//   return function(next) {
+//     return function(action) {
+//
+//     }
+//   }
+// }
